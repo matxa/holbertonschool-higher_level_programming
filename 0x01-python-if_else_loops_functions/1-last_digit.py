5589f84760cdebdba90a860cb508157e1f39e215
matxa <matxa21@gmail.com>
2020-04-28 13:49:51 -0400

    fix line lenght

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,11 @@ if (number < 0):
 last_digit = number % (module_ten)
 
 if (last_digit > 5):
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
+    print("Last digit of {:d} is {:d} \
+    and is greater than 5".format(number, last_digit))
 elif (last_digit == 0):
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+    print("Last digit of {:d} is {:d} \
+    and is 0".format(number, last_digit))
 elif (last_digit < 6 and last_digit != 0):
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+    print("Last digit of {:d} is {:d} \
+    and is less than 6 and not 0".format(number, last_digit))
