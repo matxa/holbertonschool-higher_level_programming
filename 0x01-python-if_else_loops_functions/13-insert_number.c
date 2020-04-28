a90ba3b28605321459eabcaf3990d9f8c5ce7269
matxa <matxa21@gmail.com>
2020-04-28 17:03:52 -0400

    add if statment

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,6 +13,9 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *last = *head;
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = number;
 	new_node->next = NULL;
 
