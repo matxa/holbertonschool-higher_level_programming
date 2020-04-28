963cbd7c08c8073ffd5dafa094909910723e3e0a
matxa <matxa21@gmail.com>
2020-04-28 21:20:58 +0000

    simplify if stat

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,22 +13,16 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *last = *head;
 
-	if (new_node == NULL)
+	if (new_node == NULL && *head == NULL)
 		return (NULL);
 
 	new_node->n = number;
 	new_node->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (NULL);
-	}
-
 	while (last->next != NULL && last->next->n < new_node->n)
 	last = last->next;
 
-	 new_node->next = last->next;
+	new_node->next = last->next;
 	last->next = new_node;
 
 	return (new_node);
