683c46137bc1a51a0dea471bdb596d7095d1bd5e
matxa <matxa21@gmail.com>
2020-04-28 14:55:05 -0400

    print comboi

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if (i != 99):
-        print("{:02d}, ".format(i), end='')
-    else:
-        print("{:d}".format(i))
+for num in range(0, 100):
+    if (num == 89):
+        print("{}".format(num))
+    elif (num / 10 < num % 10):
+        print("{:02d}".format(num), end=' ')
