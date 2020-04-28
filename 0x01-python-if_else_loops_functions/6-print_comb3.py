c4b7dd2da451c21a6042b6bee1acbaf95daa77df
matxa <matxa21@gmail.com>
2020-04-28 14:57:49 -0400

    print combo v1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for num in range(0, 100):
     if (num == 89):
         print("{}".format(num))
     elif (num / 10 < num % 10):
-        print("{:02d}".format(num), end=' ')
+        print("{:02d}".format(num), end=', ')
