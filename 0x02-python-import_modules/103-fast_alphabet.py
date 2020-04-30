30b4b78abd4f55280ffb1fadda0686354b23cbc4
matxa <matxa21@gmail.com>
2020-04-30 15:09:07 -0400

    ma

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-from import file_helper_103 import upper_alpha
-upper_alpha()
+import file_helper_103
+print()
--- 0x02-python-import_modules/file_helper_103.py
+++ 0x02-python-import_modules/file_helper_103.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def upper_alpha():
-    for i in range(65, 90):
-        print("{}".format(chr(i)), end='')
-    print()
+for i in range(65, 90):
+    print("{}".format(chr(i)), end='')
