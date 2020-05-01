f5aa71583dcaef22a80b47754cd0b73ccce40cdb
matxa <matxa21@gmail.com>
2020-05-01 00:01:39 -0400

    cal

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,8 +6,8 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
             return c
-        return sub(a, b)
-    return None
+
+    return sub(a, b)
 
 from dis import dis
 dis(magic_calculation)
