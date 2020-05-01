538edf10b8a84c1c113eb29916c65508323fb792
matxa <matxa21@gmail.com>
2020-05-01 00:27:51 -0400

    add new_node pointer

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,19 +11,19 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *last = *head;
+	listint_t *temp = *head;
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = number;
-    new_node->next = *head;
+	new_node->next = *head;
 
-	while (last->next != NULL && last->next->n < new_node->n)
-		last = last->next;
+	while (temp->next != NULL && temp->next->n < new_node->n)
+		temp = temp->next;
 
-	new_node->next = last->next;
-	last->next = new_node;
+	new_node->next = temp->next;
+	temp->next = new_node;
 
 	return (new_node);
 }
