3e36eeedc401b2f4b9c5fb46f31b6ae371bf8640
matxa <matxa21@gmail.com>
2020-05-02 11:45:19 -0400

    edge case

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,7 +13,12 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *temp = *head;
 
-	if (head == NULL || new_node == NULL)
+	if (head == NULL)
+	{
+		*head = new_node;
+	}
+
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = number;
