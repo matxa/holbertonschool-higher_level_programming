bdb7510435db43d61a6024114c32831c43f2d78b
matxa <matxa21@gmail.com>
2020-05-07 16:24:35 -0400

    mul v.2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,4 +5,4 @@ def multiply_by_2(a_dictionary):
         for k, v in a_dictionary_copy.items():
             a_dictionary_copy.update({k: v*2})
 
-        return a_dictionary_copy
+    return a_dictionary_copy
