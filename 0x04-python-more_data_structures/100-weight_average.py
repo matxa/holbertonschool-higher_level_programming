14fc6185e8c93374b3bf9aeccd88dec370f15e5b
matxa <matxa21@gmail.com>
2020-05-08 11:55:22 -0400

    av

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    tuple_1_idx = []
+    tuple_mul = []
+    for l in my_list:
+        for t in range(len(l)):
+            tuple_1_idx.append(l[1])
+            tuple_mul.append(l[0] * l[1])
+            break
+    total = sum(tuple_mul) / sum(tuple_1_idx)
+    return total
