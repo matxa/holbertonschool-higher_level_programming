ddbe0d64f7dddd713d74714dc8f8707217636c10
matxa <matxa21@gmail.com>
2020-05-07 22:17:33 -0400

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum_rom = []
+    total_sum = 0
+    idx = 0
+    rom_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum_rom_copy = []
+
+    if not roman_string or type(roman_string) != str:
+        return 0
+    for i in roman_string:
+        if i in rom_dic.keys():
+            sum_rom.append(rom_dic.get(i))
+    if len(sum_rom) % 2 != 0:
+        sum_rom.append(0)
+    for x, y in zip(sum_rom[::1], sum_rom[1::1]):
+        if y > x:
+            x *= -1
+        sum_rom_copy.append(x)
+        if len(sum_rom) < 3:
+            sum_rom_copy.append(y)
+    number = sum(sum_rom_copy)
+    return number
