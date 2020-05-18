fff73bb7653058bca515d0316516629bf11896d7
matxa <matxa21@gmail.com>
2020-05-18 18:27:23 -0400

    final commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        return fct(args[0], args[1])
+        return fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=stderr)
         return None
