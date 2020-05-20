46b3f91dd4fadebd7de72f9ebb19207b48bfc5e4
matxa <matxa21@gmail.com>
2020-05-19 22:19:41 -0400

    4-s

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Init class"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    """Method for calc the area of square"""
+    def area(self):
+        return self.__size * self.__size
+    """Attribute to get size"""
+    @property
+    def size(self):
+        return self.__size
+    """Attribute to set size"""
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
