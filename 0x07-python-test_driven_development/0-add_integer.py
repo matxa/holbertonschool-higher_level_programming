c5843d8da518d0fbd5951d2a9eeed6961becfdbc
matxa13 <matxa21@gmail.com>
2020-05-21 23:17:51 -0400

    backup

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """Return the sum of a and b
+    if a is not None and type is int or float
+    """
+    if a is None:
+        raise TypeError("a must be an integer")
+    if a * 0 != 0:
+        raise TypeError("a must be an integer")
+    else:
+        a = int(a)
+    if b * 0 != 0:
+        raise TypeError("b must be an integer")
+    else:
+        b = int(b)
+    return a + b
