bb68602dd4687779e1010f15ceb2ebbd9b2835dd
matxa <matxa21@gmail.com>
2020-05-25 23:25:03 -0400

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -3,9 +3,13 @@
 
 
 class Rectangle:
+    """Initializing the Rectangle class
+    """
+
+    # Public attribute
     number_of_instances = 0
-    print_symbol = "#"
-    """Initializing the Rectangle class"""
+    print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         if type(height) != int:
             raise TypeError("height must be an integer")
@@ -59,6 +63,8 @@ class Rectangle:
 
     """String Representation of rectangle class"""
     def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
         symb = (str(self.print_symbol) * self.__width)
         rect_r = (symb + '\n') * (self.__height - 1)
         return rect_r + symb
@@ -72,7 +78,6 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    """Checking for equality if > = <"""
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
