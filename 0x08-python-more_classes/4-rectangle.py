d0baf5b76c08977643f76bb44c04fae01a78ccd9
matxa <matxa21@gmail.com>
2020-05-25 21:49:55 -0400

    4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     """Getting the private __height variable"""
     @property
     def height(self):
-        return self.__width
+        return self.__height
 
     """Setting the private __height variable"""
     @height.setter
@@ -48,7 +48,7 @@ class Rectangle:
     def area(self):
         return self.__height * self.__width
 
-    """Calculate the perimiter of the rectangle"""
+    """Calculate the perimiter of the rectangle class"""
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
@@ -56,6 +56,8 @@ class Rectangle:
 
     """String Representation of rectangle class"""
     def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
         rect_r = (('#' * self.__width) + '\n') * (self.__height - 1)
         return rect_r + ('#' * self.__width)
 
