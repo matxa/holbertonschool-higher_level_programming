e2e09c29db66c2dfbeba6eb4aacf80fb19de2f3f
matxa <matxa21@gmail.com>
2020-05-26 17:43:44 -0400

    cleaning code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,10 +10,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    else:
-        a = int(a)
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    else:
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
