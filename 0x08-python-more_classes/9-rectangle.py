9ac0ffca9bf59ba4c0860087145a5f94575cdfc0
matxa <matxa21@gmail.com>
2020-05-26 00:01:55 -0400

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,11 +11,11 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        if type(height) != int:
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        if type(width) != int:
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
