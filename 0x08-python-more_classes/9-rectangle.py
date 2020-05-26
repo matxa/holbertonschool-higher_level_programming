47359665ba399dbf8537990581f561ec6873b9e3
matxa <matxa21@gmail.com>
2020-05-26 00:16:57 -0400

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -61,25 +61,6 @@ class Rectangle:
             return 0
         return (self.__height + self.__height) + (self.__width + self.__width)
 
-    """What is the biggest rect"""
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() > rect_2.area():
-            return rect_1
-        elif rect_1.area() == rect_2.area():
-            return rect_1
-        else:
-            return rect_2
-
-    """A rectangle is a square"""
-    @classmethod
-    def square(cls, size=0):
-        return cls(size, size)
-
     """String Representation of rectangle class"""
     def __str__(self):
         if self.__height == 0 or self.__width == 0:
@@ -96,3 +77,20 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        elif rect_1.area() == rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
