6c41ee4542c28172b8967d4f7ba5fe553c9976e8
matxa <matxa21@gmail.com>
2020-05-26 17:40:14 -0400

    using isinstance() instead of type()

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,11 +8,11 @@ def add_integer(a, b=98):
     """
     if a is None:
         raise TypeError("a must be an integer")
-    if a * 0 != 0:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     else:
         a = int(a)
-    if b * 0 != 0:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
         b = int(b)
