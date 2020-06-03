8414b75e808b952fe573d69230c78204b024bcf3
matxa <matxa21@gmail.com>
2020-06-03 19:28:10 -0400

    retest v.3

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,9 +6,9 @@ def read_lines(filename="", nb_lines=0):
     """opens a file and print n
     amount of lines
     """
-    with open('my_file_0.txt', encoding="utf8") as file:
+    with open('my_file_0.txt', 'rt', encoding="utf8") as file:
         text = file.readlines()
-    if nb_lines < 0 or len(text) < nb_lines:
+    if nb_lines <= 0 or len(text) < nb_lines:
         for line in text:
             print(line, end="")
     else:
