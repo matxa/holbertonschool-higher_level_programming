11e1f427968935f8f0ff76ee06b0d93ed28e0dc4
matxa <matxa21@gmail.com>
2020-06-02 23:52:35 -0400

    tdd

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Class MyList"""
 
 
 class MyList(list):
-    """My list inherits from List object
-    """
+    """My list inherits from List object"""
     def print_sorted(self):
         """Print sorted list
         """
