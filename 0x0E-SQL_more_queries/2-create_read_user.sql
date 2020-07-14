ad5786c431f11c085d63db462b86fb6409307b87
matxa13 <matxa21@gmail.com>
2020-07-14 18:50:59 -0400

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
--- create hbtn_0c_2 database
--- user_0d_2 in hbnt_0c_2 database
+-- create hbtn_0d_2 database
+-- user_0d_2 in hbnt_0d_2 database
 -- give user only select privilage
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
