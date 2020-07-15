2c97292c3ef6a013f39de6a1024ff2d3f0b34329
matxa13 <matxa21@gmail.com>
2020-07-14 22:42:30 -0400

    15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+SELECT tv_shows.title
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id AND tv_genres.name = 'Comedy'
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title;
