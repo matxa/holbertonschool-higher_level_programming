d9a2693ba59c6eb772118909347bc2e09a105351
matxa13 <matxa21@gmail.com>
2020-07-14 22:21:11 -0400

    12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- List all shows
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id is NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
