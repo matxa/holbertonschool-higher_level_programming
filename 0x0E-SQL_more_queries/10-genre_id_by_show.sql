a898dabc99495eb20cce69ced06cff6277a364f7
matxa13 <matxa21@gmail.com>
2020-07-14 22:12:17 -0400

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- List shows
-SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre.id
-FROM tv_shows JOIN tv_show_genres
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+JOIN tv_show_genres
 WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
