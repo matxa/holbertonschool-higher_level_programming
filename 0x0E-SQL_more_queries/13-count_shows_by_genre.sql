1ddf3735fd38162b4cbd7db788754fc564b6995b
matxa13 <matxa21@gmail.com>
2020-07-14 22:31:35 -0400

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
-WHERE tv_genres.id = tv_show_genres.show_id
+WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
