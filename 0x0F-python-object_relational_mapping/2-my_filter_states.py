d591c65581d64168fb4fb70fa9ee7f775f4748a4
matxa <matxa21@gmail.com>
2020-08-07 21:28:31 +0000

    using format %s

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""MySQLdb practice"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    # connect to my sql server
+    connect_db = MySQLdb.connect(host="localhost", port=3066, charset="utf8",
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+
+    # making cursor obj for execution
+    cursor_obj = connect_db.cursor()
+
+    # executing
+    cursor_obj.execute(
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC;",
+        (argv[4], ))
+
+    # fetch rows from cursor_obj
+    query_rows = cursor_obj.fetchall()
+
+    # loop through fetched rows
+    for row in query_rows:
+        print(row)
+
+    # close cursor_obj and connect_db
+    cursor_obj.close
+    connect_db.close
