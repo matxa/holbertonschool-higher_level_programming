bddb246b5f2cd00f9692a612df569a6b036a3379
matxa13 <matxa21@gmail.com>
2020-08-08 21:39:30 -0400

    change name

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""MySQLdb practice"""
+"""Write a script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
 from sys import argv
 import MySQLdb
 
