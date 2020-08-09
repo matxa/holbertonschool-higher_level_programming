9d1367bc06622121858c5d47fecc9282ebe5e3d6
matxa13 <matxa21@gmail.com>
2020-08-09 10:13:04 -0400

    add to data base

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm.exc import NoResultFound
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    session = Session(engine)
+
+    louisiana = State(name="Louisiana")
+
+    session.add(louisiana)
+    session.commit()
+    session.close()
+
+    Base.metadata.create_all(engine)
