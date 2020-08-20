9db6564d0e57f16b85c5ede3c06a3a39249cf712
matxa <42968545+matxa@users.noreply.github.com>
2020-08-19 20:59:49 -0400

    binary search algo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-""" Module that defines function find_peak
+"""Find Peak by basically using binary search
 """
 
 
+def find_peak_helper(arr, low, high, n):
+    """ Helper function """
+    mid = low + (high - low)/2
+    mid = int(mid)
+    if ((mid == 0 or arr[mid - 1] <= arr[mid]) and
+       (mid == n - 1 or arr[mid + 1] <= arr[mid])):
+        return mid
+    elif (mid > 0 and arr[mid - 1] > arr[mid]):
+        return find_peak_helper(arr, low, (mid - 1), n)
+    else:
+        return find_peak_helper(arr, (mid + 1), high, n)
+
+
 def find_peak(list_of_integers):
-    """ PeaK Finding """
-    if len(list_of_integers) == 0:
+    """ Find Peak """
+    if len(list_of_integers) <= 0:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    if len(list_of_integers) == 2:
-        if list_of_integers[0] > list_of_integers[1]:
-            return list_of_integers[0]
-        else:
-            return list_of_integers[1]
-
-    if list_of_integers.count(list_of_integers[0]) == len(list_of_integers):
-        return list_of_integers[0]
-
-    if list_of_integers[0] > list_of_integers[1]:
-        return list_of_integers[0]
-    if list_of_integers[-1] > list_of_integers[-2]:
-        return list_of_integers[-1]
-
-    peak_lis = []
-    for num in range(len(list_of_integers)):
-        if list_of_integers[num] > list_of_integers[num - 1] and\
-           list_of_integers[num] > list_of_integers[num + 1]:
-            peak_lis.append(list_of_integers[num])
-    return peak_lis[0]
+    size = len(list_of_integers)
+    index = find_peak_helper(list_of_integers, 0, size - 1, size)
+    return list_of_integers[index]
