949969e479c6ff8908f4879f943872f92431c416
matxa <42968545+matxa@users.noreply.github.com>
2020-08-24 14:21:23 -0400

    fix code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as resp:
         resp_read = resp.read()
         print("Body response:")
-        print("    - type: {}".format(type(resp_read)))
-        print("    - content: {}".format(resp_read))
-        print("    - utf8 content: {}".format(resp_read.decode('utf-8')))
+        print("	- type: {}".format(type(resp_read)))
+        print("	- content: {}".format(resp_read))
+        print("	- utf8 content: {}".format(resp_read.decode('utf-8')))
