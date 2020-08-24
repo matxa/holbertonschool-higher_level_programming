9d35fdae84996fefa24fb08e6b30da308245424c
matxa <42968545+matxa@users.noreply.github.com>
2020-08-24 14:15:03 -0400

    fix code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,10 @@ import urllib
 from urllib import request
 
 
-url = 'https://intranet.hbtn.io/status'
-with request.urlopen(url) as resp:
-    resp_read = resp.read()
-    body_resp = {
-        'type': type(resp_read),
-        'content': resp_read,
-        'utf8 content': resp_read.decode('utf-8')
-    }
-    print("Body response:")
-    for k, v in body_resp.items():
-        print("\t- {}: {}".format(k, v))
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as resp:
+        resp_read = resp.read()
+        print("Body response:")
+        print("    - type: {}".format(type(resp_read)))
+        print("    - content: {}".format(resp_read))
+        print("    - utf8 content: {}".format(resp_read.decode('utf-8')))
