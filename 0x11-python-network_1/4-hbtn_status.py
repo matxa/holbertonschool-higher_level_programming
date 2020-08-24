8606bf96cb9740e3380af6e4063fe33e44695479
matxa <42968545+matxa@users.noreply.github.com>
2020-08-24 16:46:46 -0400

    request vs urllib

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     with requests.get('https://intranet.hbtn.io/status') as res:
         print('Body response:')
         print('	- type: {}'.format(type(res.text)))
-        print('	- type: {}'.format(res.text))
+        print('	- content: {}'.format(res.text))
