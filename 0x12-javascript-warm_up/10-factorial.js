7168e4a50f66dec0a69e4cbc148576f3faf6dae0
matxa <42968545+matxa@users.noreply.github.com>
2020-09-15 20:38:43 -0400

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 // factorial
 function factorial (num) {
-  if (num === 0) {
+  if (num === 0 || !num) {
     return 1;
   }
   return num * factorial(num - 1);
 }
 
-if (parseInt(process.argv[2])) {
-  console.log(factorial(parseInt(process.argv[2])));
-}
+console.log(factorial(parseInt(process.argv[2])));
