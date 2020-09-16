b3a785857772c931532c2f2edcc93ee610d5ee71
matxa <42968545+matxa@users.noreply.github.com>
2020-09-15 23:10:00 -0400

    fix 1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,26 @@
 #!/usr/bin/node
 // factorial
+function SecondLargest (array) {
+  array.sort(function (x, y) {
+    return x - y;
+  });
+  const unique = [array[0]];
+  const result = [];
+
+  for (let j = 1; j < array.length; j++) {
+    if (array[j - 1] !== array[j]) {
+      unique.push(array[j]);
+    }
+  }
+  result.push(unique[unique.length - 2]);
+  return result.join(',');
+}
+
 if (!process.argv[2]) {
   console.log(0);
 } else if (process.argv.length === 3) {
   console.log(0);
 } else {
   const array = process.argv.slice(2, process.argv.length);
-  array.sort();
-  array.pop();
-  console.log(Math.max(...array));
+  console.log(SecondLargest(array));
 }
