ab7ab997373a909432f8a1201405cb6d348fb45b
matxa <42968545+matxa@users.noreply.github.com>
2020-09-15 21:05:18 -0400

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// factorial
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const array = process.argv.slice(2, process.argv.length);
+  const newArray = [];
+  for (let step = 0; step < array.length; step++) {
+    newArray.push(parseInt(array[step]));
+  }
+  newArray.sort();
+  console.log(newArray[newArray.length - 2]);
+}
