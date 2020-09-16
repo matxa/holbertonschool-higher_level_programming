afc7ba3998823ccb89c49a8f51385f0bf1a17864
matxa <42968545+matxa@users.noreply.github.com>
2020-09-15 21:37:57 -0400

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // factorial
-if (process.argv.length === 2 || process.argv.length === 3 || !process.argv.length) {
+if (!process.argv[2] || process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   const array = process.argv.slice(2, process.argv.length);
