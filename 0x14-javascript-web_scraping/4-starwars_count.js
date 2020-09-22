d1444cc9b68a3f33f33badfc8d16dc2030ef7e68
matxa <42968545+matxa@users.noreply.github.com>
2020-09-22 18:47:51 -0400

    manual use insted of find

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -13,9 +13,11 @@ request(options, (err, res, body) => {
     const data = JSON.parse(body);
     const characters = [];
     for (let i = 0; i < data.results.length; i++) {
-      const WedgeAntilles = data.results[i].characters.find(check => check === 'https://swapi-api.hbtn.io/api/people/18/');
-      if (WedgeAntilles) {
-        characters.push(WedgeAntilles);
+      const res = data.results[i].characters;
+      for (let r = 0; r < res.length; r++) {
+        if (res[r] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          characters.push(res[r]);
+        }
       }
     }
     console.log(characters.length);
