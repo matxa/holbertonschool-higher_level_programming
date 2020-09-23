3ffba203b1dee940b9409681c684448d5c366828
matxa <42968545+matxa@users.noreply.github.com>
2020-09-23 02:29:50 -0400

    add alternative url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,7 +10,7 @@ request(process.argv[2], (err, res, body) => {
     for (let i = 0; i < data.results.length; i++) {
       const res = data.results[i].characters;
       for (let r = 0; r < res.length; r++) {
-        if (res[r] === 'https://swapi-api.hbtn.io/api/people/18/') {
+        if (res[r] === 'https://swapi-api.hbtn.io/api/people/18/' || res[r] === 'http://swapi.co/api/people/18/') {
           characters.push(res[r]);
         }
       }
