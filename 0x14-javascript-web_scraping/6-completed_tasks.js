0c9aec3c2e5fb4e14120d01ec9baf1dbcd581542
matxa <42968545+matxa@users.noreply.github.com>
2020-09-22 20:08:16 -0400

    get users length from api

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,22 +8,27 @@ const options = {
   method: 'GET'
 };
 
-request(options, (err, res, body) => {
+request('https://jsonplaceholder.typicode.com/users', (err, res, body) => {
   if (!err && res.statusCode === 200) {
-    const data = JSON.parse(body);
-    let taksCount = 0;
-    const log = {};
-    for (let user = 1; user <= 10; user++) {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].userId === user) {
-          if (data[i].completed) {
-            taksCount += 1;
+    const userAmount = JSON.parse(body).length;
+    request(options, (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        const data = JSON.parse(body);
+        let taksCount = 0;
+        const log = {};
+        for (let user = 1; user <= userAmount; user++) {
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].userId === user) {
+              if (data[i].completed) {
+                taksCount += 1;
+              }
+            }
           }
+          log[user] = taksCount;
+          taksCount = 0;
         }
+        console.log(log);
       }
-      log[user] = taksCount;
-      taksCount = 0;
-    }
-    console.log(log);
+    });
   }
 });
